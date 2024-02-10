06662e0ab0715fc3f544ae1db1f9af26aed3328e
Jill <jillee27@hotmail.com>
2024-02-09 20:22:45 -0800

    add transition image to pop-out

--- lesson04/assignment/js/script.js
+++ lesson04/assignment/js/script.js
@@ -1,22 +1,25 @@
-// Get the modal
-var modal = document.getElementById('welcomeModal');
+document.addEventListener('DOMContentLoaded', function() {
+  var modal = document.getElementById('welcomeModal');
+  var span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+ // Open the modal with a transition
+ modal.classList.add('modal-visible'); // Use class to control visibility and apply transition
 
-// When the page loads, open the modal 
-window.onload = function() {
+  // Open the modal
   modal.style.display = "block";
+
+  // When the user clicks on <span> (x), close the modal
+  if (span) {
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  // When the user clicks anywhere outside of the modal, close it smoothly
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.classList.remove('modal-visible');
+    }
 }
+});
