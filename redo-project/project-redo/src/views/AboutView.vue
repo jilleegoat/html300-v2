17332bb2587193aea5ce61cfc3f21c94c3c7c1ac
Jill <jillee27@hotmail.com>
2024-03-18 08:02:23 -0700

    upload for review of lesson08

--- redo-project/project-redo/src/views/AboutView.vue
+++ redo-project/project-redo/src/views/AboutView.vue
@@ -3,22 +3,18 @@
     <h1 v-if="isLoggedIn">Welcome back, user!</h1>
     <h1 v-else>Please log in to continue.</h1>
     <button v-if="!isLoggedIn" @click="login">Login</button>
-     <!-- Image gallery -->
-     <div v-if="isLoggedIn">
+
+    <!-- Image gallery -->
+    <div v-if="isLoggedIn" class="image-gallery">
       <h2>Your Images</h2>
-      <div v-for="(image, index) in computedImages" :key="index">
-    <img :src="image" :alt="'Image ' + (index + 1)">
+      <div v-for="(image, index) in computedImages" :key="index" class="image-wrapper">
+        <image-component
+          :src="image"
+          :alt="'Image ' + (index + 1)"
+          :title="'Image ' + (index + 1)"
+        />
       </div>
     </div>
-    <div>
-      <image-component
-  v-if="isLoggedIn"
-  :src="require('../assets/02.jpg')"
-  alt="Image description"
-  title="Title of Image"
-/>
-
-  </div>
   </div>
 </template>
 
@@ -52,3 +48,22 @@ export default {
   }
 }
 </script>
+
+<style>
+.image-gallery {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center; 
+}
+
+.image-wrapper {
+  margin: 1rem;
+  max-width: 500px; /* Adjust this value to control the size of images */
+}
+
+.image-wrapper img {
+  width: 100%;
+  height: auto; /* This maintains the aspect ratio of the images */
+  object-fit: cover; /* This ensures that the images cover the area without stretching */
+}
+</style>
